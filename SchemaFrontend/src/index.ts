52a2a4ec32f63e3ab16e6dd725f2a906fb7b6417
Westin Miller <equinoxscripts@gmail.com>
2024-06-04 05:06:40 -0700

    easier lazy loading

--- SchemaFrontend/src/index.ts
+++ SchemaFrontend/src/index.ts
@@ -1,9 +1,4 @@
-import "ace-code/src/theme/monokai";
-import { SchemaIr } from "./ir";
 import { locationParameters } from "./util";
-import { XmlBuilder } from "./xml";
-import { generateExample } from "./generate";
-import { AceTooltips } from "./tooltip";
 
 (function () {
     const editorDom = document.createElement("editor");
@@ -23,29 +18,5 @@ import { AceTooltips } from "./tooltip";
     const stripPrefix = "MyObjectBuilder_";
     document.title = root.length == 2 ? root[1].startsWith(stripPrefix) ? root[1].substring(stripPrefix.length) : root[1] : root[0];
 
-    import('ace-code').then(ace => {
-        const editor = ace.edit(editorDom);
-        editor.setReadOnly(true);
-        const tooltips = new AceTooltips(editor);
-
-        import('ace-code/src/mode/xml').then(xml => editor.session.setMode(new xml.Mode()));
-        import('ace-code/src/theme/monokai').then(theme => {
-            editor.setStyle(theme.cssClass);
-            const styleRef = document.getElementById(theme.cssClass);
-            // Move to end so the theme takes priority.
-            styleRef.parentElement.appendChild(styleRef);
-        });
-
-        const schemaPath = "https://storage.googleapis.com/unofficial-keen-schemas/latest/" + schema[0];
-        fetch(schemaPath + ".json")
-            .then(response => response.json())
-            .then(json => json as SchemaIr)
-            .then(ir => {
-                const builder = new XmlBuilder({ editor, tooltips, schema: schemaPath + ".xsd" });
-                generateExample(ir, builder, path);
-            })
-            .catch(err => {
-                console.warn("Failed to load schema IR for " + schema[0], err);
-            });
-    });
+    import('./editor').then(editor => editor.setupEditor(editorDom, schema[0], path));
 })();
