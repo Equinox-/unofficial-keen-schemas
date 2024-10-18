4b6c6058a255fcf56dafc282a36aafcc0a27e59b
Westin Miller <equinoxscripts@gmail.com>
2024-10-18 13:00:12 -0700

    Remove data extractor

--- SchemaBuilder/Program.cs
+++ SchemaBuilder/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
-using SchemaBuilder.Data;
 using SchemaBuilder.Schema;
 using SchemaService.SteamUtils;
 using SteamKit2;
@@ -20,9 +18,7 @@ namespace SchemaBuilder
                     svc.AddSingleton<GameManager>();
                     svc.AddHostedService<GameManager>();
                     svc.AddSingleton<SchemaGenerator>();
-                    svc.AddSingleton<DataExtractor>();
                     svc.AddSingleton<DocReader>();
-                    svc.AddSingleton<WikiWriter>();
                 })
                 .Build();
             await host.StartAsync();
@@ -34,18 +30,6 @@ namespace SchemaBuilder
                     {
                         var schemas = host.Services.GetRequiredService<SchemaGenerator>();
                         await schemas.Generate(args[1]);
-                        break;
-                    }
-                    case "data":
-                    {
-                        var data = host.Services.GetRequiredService<DataExtractor>();
-                        await data.Generate(args[1]);
-                        break;
-                    }
-                    case "wiki":
-                    {
-                        var writer = host.Services.GetRequiredService<WikiWriter>();
-                        await writer.Write(args[1]);
                         break;
                     }
                     default:
